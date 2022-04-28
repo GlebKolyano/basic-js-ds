d55114d2d409326c83a53705f21849ad6680b55c
GlebKolyano <glebkolyano38@gmail.com>
2022-04-28 09:32:32 +0300

    feat: solve the task stack

--- src/stack.js
+++ src/stack.js
@@ -13,22 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  array = []
+
+  constructor() {}
+
+  push(el) {
+    this.array.push(el)
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  pop(el) {
+    return this.array.pop(el)
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array[this.array.length - 1]
   }
 }
+
 
 module.exports = {
   Stack
 };
+
+
+
